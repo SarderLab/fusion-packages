fbd329c87ad09302e519f922e7dd095e19d3c1ba
srisharanya Injarapu <injarapu.s@ufl.edu>
2026-01-15 10:07:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup
+from setuptools.command.develop import develop
 from setuptools.command.install import install
 import subprocess
 import sys
 
+def run_download():
+    subprocess.call([sys.executable, '-m', 'fusion.download_notebooks'])
+
+class PostDevelopCommand(develop):
+    def run(self):
+        develop.run(self)
+        run_download()
+
 class PostInstallCommand(install):
     def run(self):
         install.run(self)
-        # Run the notebook download after installation
-        subprocess.call([sys.executable, '-m', 'fusion.download_notebooks'])
+        run_download()
 
 setup(
-    cmdclass={'install': PostInstallCommand}
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    }
 )
