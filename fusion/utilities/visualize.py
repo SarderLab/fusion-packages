05e83f1d76f25f9942638dcb4d0520cd67a035bd
srisharanya Injarapu <injarapu.s@ufl.edu>
2026-01-18 17:26:31 -0500

    Add visualization function for HistomicUI

--- fusion/utilities/visualize.py
+++ fusion/utilities/visualize.py
@@ -2,12 +2,27 @@ import os
 import requests
 import large_image
 from fusion.utilities.utility import get_hubmap_url
+from IPython.display import IFrame
+
+# vizualise user's data
+def vizualise_in_histomicUI(item_id: str):
+    if not item_id:
+        raise ValueError("please provide a valid item_id")
+    else:
+        try:
+            ts =IFrame(f"https://athena.rc.ufl.edu/histomics#?image={item_id}", "100%", "800px")
+        except Exception:
+            raise
+    return ts
+
+ts = vizualise_in_histomicUI(response['item_id'])
+ts
 
 def visualize_hubmap_wsi(hubmap_id,overwrite=False):
     url = get_hubmap_url(hubmap_id)
     print("URL:", url)
     filename = os.path.basename(url)
-    print(filename)
+
 
     if os.path.exists(filename) and not overwrite:
         print(f"{filename} already exists. Skipping download.")
@@ -20,4 +35,5 @@ def visualize_hubmap_wsi(hubmap_id,overwrite=False):
                     f.write(chunk)
     
     ts = large_image.open(filename)
+
     return ts
