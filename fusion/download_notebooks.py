f97eaa1d331602fe0d91cdad6c640c29e4ae0bbc
Durga Sritha Dongla <durgasrithadongla@ssrb-vpn2-10-33.vpn.ufl.edu>
2026-04-02 16:55:43 -0400

    change notebook path to point to main branch

--- fusion/download_notebooks.py
+++ fusion/download_notebooks.py
@@ -7,7 +7,7 @@ def download_demo_notebooks():
     notebooks_dir.mkdir(parents=True, exist_ok=True)
     
     # GitHub raw URLs
-    base_url = "https://raw.githubusercontent.com/SarderLab/fusion-packages/Fusion++_Demo/fusion/"
+    base_url = "https://raw.githubusercontent.com/SarderLab/fusion-packages/main/fusion/"
     notebooks = [
         "fusion_demo.ipynb"
     ]
