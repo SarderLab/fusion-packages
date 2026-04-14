e0fbd145fa45bbd58655977a1eebd0a1997b6464
srisharanya Injarapu <injarapu.s@ufl.edu>
2026-04-14 12:22:54 -0400

    change path for file download from FUSION

--- fusion/utilities/utility.py
+++ fusion/utilities/utility.py
@@ -14,7 +14,7 @@ def download_from_fusion(gc, resource_id, resource_type="file", output_dir="."):
         
         # Use the file name as the folder name
         folder_name = os.path.splitext(file_name)[0]
-        folder_path = f"./{folder_name}"
+        folder_path = f".datasets/{folder_name}/image"
         os.makedirs(folder_path, exist_ok=True)
         
         # Set the final output path inside the new folder
